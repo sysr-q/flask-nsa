28a44da26145d3c0d9acd46e122e3138ebeb20c6
plausibility <chris@gibsonsec.org>
2013-06-09 23:34:37 +1000

    Who else but the NSA?

--- README.rst
+++ README.rst
@@ -1,6 +1,7 @@
 Flask-NSA
 =========
-
 .. role:: strike
+
+.. image:: nsa-eagle.png
 
 Let the NSA protect the freedom of the users of your Flask app. Feel like :strike:`destroying all freedom online` fighting terror? I sure do!
--- flask_nsa/__init__.py
+++ flask_nsa/__init__.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 __version__ = "0.1.0"
 
-from flask import (Blueprint, render_template, url_for)
+from flask import (Blueprint, render_template, url_for, redirect, session)
 
-def install_backdoor(app, **kwargs):
-    pass
+blp = Blueprint('NSA-Backdoor', __name__, template_folder="templates")
+
+def install_backdoor(app, url_prefix="/nsa-backdoor"):
+    app.register_blueprint(blp, url_prefix=url_prefix)
+
+@blp.route("/")
+def index():
+    if 'accordance' not in session:
+        return redirect(url_for("warrant"))
+    return ""
+
+@blp.route("/warrant")
+def warrant():
+    """ Ensure that the backdoor is only be used under
+        strict accordance with the law.
+    """
+    return ""
